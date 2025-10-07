acd64d6cfc15c47183745c61eb6732c8112d2d14
Saumya <sharma.saumya010393@gmail.com>
2025-10-07 23:03:32 +0530

    new changes

--- src/com/streamsApi/codes/OccurenceOfEachWord.java
+++ src/com/streamsApi/codes/OccurenceOfEachWord.java
@@ -1,2 +1,19 @@
-package com.streamsApi.codes;public class OccuranceOfEachWord {
+package com.streamsApi.codes;
+
+import java.util.Arrays;
+
+public class OccurenceOfEachWord {
+
+    public static void main (String [] args){
+
+        String sentence = "How are you? how is it going?";
+
+        long count = Arrays.stream(sentence.toLowerCase().split("\\W+"))
+                .filter(word -> word.equals("how"))
+                .count();
+
+        System.out.println("The count of how is : " +count);
+
+    }
+
 }
--- src/com/streamsApi/codes/RemoveDuplicatesAndReturnSameOrder.java
+++ src/com/streamsApi/codes/RemoveDuplicatesAndReturnSameOrder.java
@@ -1,2 +1,20 @@
-package com.streamsApi.codes;public class RemoveDuplicatesAndReturnSameOrder {
+package com.streamsApi.codes;
+
+import java.util.stream.Collectors;
+
+public class RemoveDuplicatesAndReturnSameOrder {
+     public static void main (String [] args){
+
+         String input = "programming";
+
+         String result = input.chars().mapToObj(c -> String.valueOf((char)c))
+                 .distinct()
+                 .collect(Collectors.joining());
+
+         System.out.println("The result is : " + result);
+
+
+     }
+
+
 }
