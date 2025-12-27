8935661dfb44c57ddc807c15dc266c6b7fb2e59b
Saumya <sharma.saumya010393@gmail.com>
2025-12-27 17:00:24 +0530

    codes commit1

---
+++ src/com/streamsApi/codes/StringPalindrome.java
@@ -0,0 +1,27 @@
+package com.streamsApi.codes;
+
+public class StringPalindrome {
+
+    public static void main(String[]args){
+
+        String s = "madam";
+        int left=0;
+        int right = s.length()-1;
+
+        boolean isPalindrome = true;
+
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                isPalindrome=false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.println(isPalindrome);
+
+
+
+    }
+}
